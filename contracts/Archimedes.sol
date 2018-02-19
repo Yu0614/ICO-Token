7ad8f778baaafbeebfe26242937935e15812c4ad
Yu0614 <azu369yu@gmail.com>
2018-02-19 15:09:55 +0900

    02-19-15-09

--- contracts/Archimedes.sol
+++ contracts/Archimedes.sol
@@ -1,7 +1,17 @@
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.18;
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
-contract Archimedes {
-  function Archimedes() {
+
+contract Archimedes is StandardToken {
+  
+    string public name = "Archimedes";
+    string public symbol = "ARC";
+    uint public decimals = 18;
+  
     // constructor
-  }
+    function Archimedes(uint initialSupply) public { 
+        totalSupply_ = initialSupply;
+        balances[msg.sender] = initialSupply;
+
+    }
 }
