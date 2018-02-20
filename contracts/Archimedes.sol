b40cd9137384d050b61603286368f9975471bf39
Yu0614 <azu369yu@gmail.com>
2018-02-20 15:52:49 +0900

    2018-02-20-15:52

--- contracts/Archimedes.sol
+++ contracts/Archimedes.sol
@@ -8,11 +8,52 @@ contract Archimedes is StandardToken {
     string public symbol = "ARC";
     uint public decimals = 18;
     //decimals = 3, 1000 symbols to see 1.00 in Mist or Ethereum Wallet, if you have only 1symbols you will see 0.001.
-  
-    // constructor
-    function Archimedes(uint initialSupply) public { 
-        totalSupply_ = initialSupply;
-        balances[msg.sender] = initialSupply;
+    uint public maxSup;
+    uint public totalSup;
+    address public owner;
+    mapping (address=>string) public thanksMessage;
 
+    /*************************************
+    *     // function Archimedes
+    *************************************/ 
+    function Archimedes(uint initialSupply, uint maxSupply) public {// constructor
+        owner = msg.sender;
+        maxSup = maxSupply;
+        totalSup = initialSupply;
+        balances[owner] = initialSupply; // （オーナー）すべてのトークンを渡す
+    } 
+
+    /*************************************
+    *       // function addTotalSupply
+    *************************************/ 
+    function addTotalSupply(uint256 _value) public {
+        require(owner == msg.sender);
+        require(maxSup >= (totalSup + _value));
+
+        totalSup += _value;
+        balances[msg.sender] += _value;
+        
     }
+    
+    /*************************************
+    *   // ありがとうメッセージを送る(set)と、100ARCも一緒に送られる(transfer)
+    *************************************/ 
+    function thanks(address _to, string _message) public {
+        thanksMessage[_to] = _message;
+        transfer(_to, 100e18);
+    }
+    
+    /*************************************
+    *   // 最新ありがとうメッセージを見る
+    *************************************/ 
+    function thanksMessage(address _address) public constant returns (string) {
+        return thanksMessage[_address];
+    }
+
+
+
+
+
 }
+
+
--- migrations/2_deploy_Archimedes.js
+++ migrations/2_deploy_Archimedes.js
@@ -1,6 +1,15 @@
 const Archimedes = artifacts.require("./Archimedes.sol");
 
 module.exports = function(deployer) {
-  let initialSupply = 5000e18
-  deployer.deploy(Archimedes,initialSupply);
+  let initialSupply = 50000e18
+  let maxSupply = 100000e18
+  deployer.deploy(Archimedes,initialSupply,maxSupply);
 };
+
+
+/*
+./ArchimedesCrowdSale.sol
+  const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1 // one second in the future
+  const endTime = startTime + (60 * 10) // 10 minutes
+  const rate = new web3.BigNumber(1000) // Exchange rate 1Eth:1000Arc
+  const wallet = accounts[0] // Eth wallets of this contract*/ 
